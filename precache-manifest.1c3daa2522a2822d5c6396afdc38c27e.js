663192654cd9450dc683ccba0e257c78ae28e076
fiberthrone-ios7-input-test.github.io <fiberthrone@yandex-team.ru>
2018-12-18 14:04:44 +0300

    Updates

---
+++ precache-manifest.1c3daa2522a2822d5c6396afdc38c27e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "434ba1d4751c7c85f89b",
+    "url": "/static/js/main.434ba1d4.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "434ba1d4751c7c85f89b",
+    "url": "/static/css/main.7c7d947c.chunk.css"
+  },
+  {
+    "revision": "1dce93159649264ab3b4a41bee4f4d1b",
+    "url": "/index.html"
+  }
+];
